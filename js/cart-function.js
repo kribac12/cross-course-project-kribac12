cf6d5cbd7026305f7017c137ff57501527189074
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-03-24 15:15:23 +0100

    worked on cart function, made men.html, added js to bestseller images

--- js/cart-function.js
+++ js/cart-function.js
@@ -1,4 +1,6 @@
-/*Got some help from Marius Solheim*/
+/*Got some help from Marius Solheim, and was 
+also inspired by this https://www.youtube.com/watch?v=YeFzkC2awTM and 
+this https://vimeo.com/457504978/726aababe6 */
 
 const addToCartButton = document.querySelector(".add-to-cart-button");
 
--- js/cart.js
+++ js/cart.js
@@ -1,19 +1,36 @@
 const totalJackets = document.querySelector(".total-jackets");
 const cartContainer = document.querySelector(".cart-container");
+const removeJacketButton = document.querySelector(".cta-remove");
 
 function addJackets() {
   let cartJackets = localStorage.getItem("jacketsAddedToCart");
   cartJackets = JSON.parse(cartJackets);
 
-  totalJackets.innerHTML = `<h2 class="total-jackets-heading">(${cartJackets.length})</h2>`;
+  totalJackets.innerHTML = `<h2 class="total-jackets-heading">Jackets in cart (${cartJackets.length})</h2>`;
 
   cartContainer.innerHTML = "";
 
   for (let i = 0; i < cartJackets.length; i++) {
     cartContainer.innerHTML += `<div class= "jacket-specific"><div class="jacket-specific-image">
         <img src="${cartJackets[i].image}"/></div><div class="jacket-specific-info"><h3>${cartJackets[i].name}</h3>
-        <p class="orange-price">${cartJackets[i].price}$</p></div></div>`;
+        <p class="orange-price">Price: ${cartJackets[i].price}$</p></div><div><button class="cta cta-remove" type="button">Remove</button></div>`;
   }
 }
 
 addJackets();
+
+/* Tried to make this work 
+
+const removeJacketButton = document.querySelector(".cta-remove");
+
+function removeJackets() {
+  for (let i = 0; i < removeJacketButton.length; i++) {
+    const removeButton = removeJacketButton[i];
+    removeButton.addEventListener("click", function (event) {
+      const removeButtonClicked = event.target;
+      console.log("clicked");
+    });
+  }
+}
+
+removeJackets(); */
