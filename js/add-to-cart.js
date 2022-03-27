94ce165c53095826faa1f71cac28a74c243905c6
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-03-27 18:41:34 +0200

    Popup-changes

--- js/add-to-cart.js
+++ js/add-to-cart.js
@@ -39,8 +39,8 @@ const popUp = document.querySelector(".popup");
 
 function successPopup() {
   try {
-    jacketContainer.innerHTML += `<div class="popup"><h3>Jacket added to bag!</h3><div class="jacket-image"><img src="${jacket.image}" alt=${jacket.name}/></div> <div class="jacket-information">
-    <p>${jacket.name}</p><p class="orange-price">${jacket.price}$</p>
+    jacketContainer.innerHTML += `<div class="popup"><div class="jacket-image"><img src="${jacket.image}" alt=${jacket.name}/></div> <div class="jacket-information">
+    <p class"popup-para">${jacket.name} was successfully added to bag!</p><p class="orange-price">${jacket.price}$</p>
     <a class="cta cta-white cta-popup" href="index.html">Shop more</a>
     <a class="cta cta-white cta-popup" href="checkout-bag.html">Go to cart</a>
     </div>
