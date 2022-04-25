9445029a6317a04be588348c592664e84c51e2ec
kribac12 <89196018+kribac12@users.noreply.github.com>
2022-04-25 11:57:52 +0200

    Working on REST API

--- js/jacket-specific.js
+++ js/jacket-specific.js
@@ -2,14 +2,28 @@ const jacketContainer = document.querySelector(".jacket-container");
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const jacketId = params.get("id");
-const jacket = jackets.find(({ id }) => id === parseInt(jacketId));
 
-function showJacket() {
+const cors = "https://noroffcors.herokuapp.com/";
+const siteUrl = "http://rainydays.local/wp-json/wc/store/products" + id;
+
+const url = cors + siteUrl;
+
+console.log(url);
+
+async function showJacket() {
   try {
+    const response = await fetch(url);
+    const results = await response.json();
+    const products = results[0];
+    console.log(products);
+
     document.title = "";
-    document.title = `${jacket.name}`;
-    jacketContainer.innerHTML += `<div class="jacket-image"><img src="${jacket.image}" alt="${jacket.name}"/></div> <div class="jacket-information">
-<h1>${jacket.name}</h1><p class="orange-price">${jacket.price}$</p><p>Description: ${jacket.description}</p><p>Materials: ${jacket.materials}</p><p>Details: ${jacket.details}</p> 
+    document.title = `${products.name}`;
+
+    const productImg = products[i].images;
+
+    jacketContainer.innerHTML += `<div class="jacket-image"><img src="${productImg[0].src}" alt="${products.name}"/></div> <div class="jacket-information">
+<h1>${products.name}</h1><p class="orange-price">$${products[i].prices.price}</p><p>Description: ${products.description}</p><p>Materials: 60% cotton, 40% polyester</p>
 <label for="sizes">Choose size</label>
 <select name="sizes" id="sizes" class="sizes">
   <option value="XXS">XSS</option>
@@ -20,7 +34,7 @@ function showJacket() {
   <option value="XL">XL</option>
   <option value="XXL">XXL</option>
 </select>
-<button class="cta cta-white add-to-cart-button" data-product=${jacket.id}>Add to cart</button></div>`;
+<button class="cta cta-white add-to-cart-button" data-product=${products.id}>Add to cart</button></div>`;
   } catch (error) {
     jacketContainer.innerHTML = alert("Could not find the jacket, try again");
   }
